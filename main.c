ce7ab78c3f5a1140844de0e0b47d9f9926717562
Kirill <salatmail86@gmail.com>
2026-06-10 21:34:35 +0700

    added_addToHead/printList

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+//Структура узла
 struct point
 {
     int x;
@@ -11,9 +12,75 @@ struct point
     struct point* prev;
 };
 
+//Функции
+
+//Добавить в голову
+bool addToHead(struct point** head, int x, int y)
+{
+    bool res = false;
+
+    if (head)
+    {
+        struct point *new_head = (struct point*)malloc(sizeof(struct point));
+
+        if (new_head)
+        {
+            new_head -> x = x;
+            new_head -> y = y;
+            new_head -> next = *head;
+            new_head -> prev = NULL;
+
+            if (*head)
+            {
+                (*head) -> prev = new_head;
+            }
+
+            *head = new_head;
+
+            res = true;
+        }
+    }
+
+    return res;
+}
+
+//Вывод содержимого списка
+void printList(struct point* head)
+{
+    if (head)
+    {
+        struct point *ptrIx = NULL;
+        ptrIx = head;
+        
+        size_t i = 0;
+
+        while (ptrIx)
+        {
+            printf("%d element: x = %d y = %d\n", i, ptrIx->x, ptrIx->y);
+            //printf("prev = %d next = %d\n", ptrIx -> prev, ptrIx -> next);
+            
+            ptrIx = ptrIx -> next;
+            i++;
+        }
+    }
+    else
+    {
+        printf("list is empty\n");
+    }
+}
+
 int main()
 {
-    
+    struct point *list_head = NULL;
+
+    printList(list_head);
+
+    printf("\n");
+
+    bool check = addToHead(&list_head, 21, 23);
+    check = addToHead(&list_head, 22, 28);
+
+    printList(list_head);
 
     return 0;
 }
