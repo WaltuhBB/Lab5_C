8763694341e3a7900d10be4d960ec9d34bbd1fb5
Kirill <salatmail86@gmail.com>
2026-06-10 19:55:34 +0700

    added_struct_point

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+struct point
+{
+    int x;
+    int y;
+
+    struct point* next;
+    struct point* prev;
+};
 
 int main()
 {
-    printf("Hello World");
+    
 
     return 0;
 }
